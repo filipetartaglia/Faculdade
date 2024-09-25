58789b70e403c9101205ba529c342f9da2bb24b7
Filipe Tartaglia <lipetartaglia9@gmail.com>
2024-09-25 11:02:54 -0300

    Avançando em Java

---
+++ Estudos-Java/Atividades/Atividade1.java
@@ -0,0 +1,12 @@
+package Atividades;
+
+public class Atividade1 {
+    public static void main (String entrada[]) {
+        int idade = 47;
+        char genero = 'M';
+        double peso = 61.5;
+        String nome = "Tartaglia";
+        String curso = "Ciência da Computação";
+    
+    }
+}
---
+++ Estudos-Java/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+class PrimeiroPrograma
+{
+    public static void main (String entrada[]) 
+    {
+        int inteiro = 47;
+        char caracter = 'F';
+        double real = 1.65;
+        String frase = "Filipe Tartaglia";
+        boolean VF = true;
+
+        if (VF == true)
+        {
+            System.out.println("Eu sou o " + frase + " tenho " + inteiro + " anos e tenho " + real + "m de altura");
+        } 
+
+        System.exit(0);
+    }
+}
