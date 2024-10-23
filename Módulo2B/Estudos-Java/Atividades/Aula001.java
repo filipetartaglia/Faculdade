d251741be0dc6a60644f33f4b8240da5e55bf183
Filipe Tartaglia <lipetartaglia9@gmail.com>
2024-10-23 09:48:01 -0300

    Aprendendo Java

---
+++ Módulo2B/Estudos-Java/Atividades/Aula001.java
@@ -0,0 +1,14 @@
+public class Aula003 {
+
+    public static void main(String[] args){
+    
+        int idade = 19;
+
+        double peso = 62.4;    
+    
+        System.out.println(idade);
+        System.out.println(peso);
+
+    }
+
+}
---
+++ Módulo2B/Estudos-Java/Exemplo001.java
@@ -0,0 +1,10 @@
+public class Exemplo001 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Filipe Tartaglia!");        
+
+    }
+        
+}
+
