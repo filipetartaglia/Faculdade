bc773a09c92f2052fe38b6e3f4c18d3b165d6482
Filipe Tartaglia <lipetartaglia9@gmail.com>
2024-10-30 10:39:55 -0300

    Estudando Switch

--- Módulo2B/Estudos-Java/Aula08/src/App.java
+++ Módulo2B/Estudos-Java/Aula08/src/App.java
@@ -3,9 +3,32 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         
-        Scanner 
-
         double nota1, nota2, nota3, media;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite sua primeira nota: ");
+        nota1 = input.nextDouble();
+
+        System.out.println("Digite sua segunda nota: ");
+        nota2 = input.nextDouble();
+
+        System.out.println("Digite sua terceira nota: ");
+        nota3 = input.nextDouble();
     
+        media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("A sua media foi de " + media);
+
+        if(media >= 7) {
+            System.out.println("Você está Aprovado!");
+        } else {
+            if(media < 4) {
+                System.out.println("Você está Reprovado!");
+            } else {
+                System.out.println("Você está em Prova Final");
+            }
+        }
+
     }
 }
---
+++ Módulo2B/Estudos-Java/Aula09/src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) {
+
+        int numero = 4;
+
+        switch(numero) {
+
+        case 1:
+            System.out.println("Você digitou 1");
+            break;
+
+        case 2:
+            System.out.println("Você digitou 2");
+            break;
+
+        case 3:
+            System.out.println("Você digitou 3");
+            break;
+
+        default:
+            System.out.println("Você digitou um valor invalido");
+        }
+
+    }
+}
