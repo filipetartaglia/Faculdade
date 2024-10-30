534bbdf9b5510b492086065c57a6f2dd2141aa3f
Filipe Tartaglia <lipetartaglia9@gmail.com>
2024-10-30 10:14:52 -0300

    Estudando if e else no Java

---
+++ Módulo2B/Estudos-Java/Aula07/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+
+        int valor = 9;
+
+        if(valor == 8) {
+            System.out.println("O valor é igual a 8");
+        } else {
+            if(valor > 8) {
+                System.out.println("O valor é maior que 8");
+            } else {
+                System.out.println("O valor é menor que 8");
+            }
+        }
+
+    }
+}
---
+++ Módulo2B/Estudos-Java/Aula08/src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        
+        Scanner 
+
+        double nota1, nota2, nota3, media;
+    
+    }
+}
