ae363e47a93cae76b953180a41821b3b215528cb
Filipe Tartaglia <lipetartaglia9@gmail.com>
2025-05-14 20:27:12 -0300

    Estudando funcoes e classes

--- Módulo3B/Desenvolvimento_Mobile/aula_3_dart/aula_2_funcoes.dart
+++ Módulo3B/Desenvolvimento_Mobile/aula_3_dart/aula_2_funcoes.dart
@@ -33,6 +33,7 @@ void main() {
 // 1. Função com Retorno
 // Função que recebe dois números inteiros 'a' e 'b' e retorna a soma deles
 
+/*
 int add(int a, int b) {
   return a + b;
 }
@@ -41,4 +42,53 @@ void main() {
   // Chama a função 'add' e passa os números 3 e 5 como argumentos e armazena eles na variável 'result'
   int result = add(3, 5);
   print('Result: $result');
+}
+*/
+
+/*
+// Função anônima
+ void main() {
+  // Declaração de uma lista de frutas usando a palavra-chave 'var'
+  // A lista contém três itens: 'apples', 'bananas', e 'oranges'
+  var list = ['apples', 'bananas', 'oranges'];
+
+  // O método 'forEach' percorre cada item da lista 
+  // Para cada item, ele executa o bloco de código que está dentro das chaves
+  list.forEach((item) {
+    print('Item: $item');
+  });
+}    
+*/
+
+/*
+// Arrow Function
+// Função que recebe dois números inteiros e retorna o produto deles
+int multiply(int a, int b) => a * b;
+// O '=>' é uma forma curta de escrever funções que retornam um valor.
+
+void main() {
+  print('Multiply: ${multiply(4, 5)}');
+}
+*/
+
+// Métodos das Classes 
+// 1. Métodos de Instância
+
+class Person {
+  String name;
+  int age;
+
+  // Recebe dois parâmetros 
+  Person(this.name, this.age);
+
+  // Método da classe que exibe a mensagem de apresentação 
+  void introduce() {
+    print('Hello, my name is $name and I am $age years old.');
+  }
+}
+
+void main() {
+  // Cria uma instância da classe 'Person' chamada 'person' 
+  var person = Person('Filipe', 19);
+  person.introduce();
 }
